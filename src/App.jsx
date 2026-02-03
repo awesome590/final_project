f940c7245b88d2db3541f8bdec59b6e8cdfcaec1
Reece <peardon.reece@g,ail.com>
2026-02-02 21:49:03 -0800

    pushing test changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from 'react'
 import Home from "./pages/Home.jsx";
 import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-        <Nav />
-        <Home />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Nav/>
+            <Home/>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <Footer/>
+        </>
+    )
 }
 
 export default App
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {Link} from 'react-dom'
+import {reactLogo} from '../assets/react.svg'
 
-function Nav() {
+const Nav = ({numberOfItems}) => {
+    function openMenu() {
+        document.body.classList += ' menu--open'
+    }
+
+    function closeMenu() {
+        document.body.classList.remove('menu--open')
+    }
+
     return (
-        <div>Nav</div>
+        <nav>
+            <div className="nav__container">
+                <Link to="/">
+                    <img src={reactLogo} alt="" className='logo'/>
+                </Link>
+                <ul className='nav__links'>
+                    <li className='nav__list'>
+                        <Link to="/" className='nav__link'>Home</Link>
+                    </li>
+                    <li className='nav__list'>
+                        <Link to="/books" className='nav__link'>Books</Link>
+                    </li>
+                    <button className='btn__menu' onClick={openMenu}>
+                    </button>
+                    <li className="nav__icon">
+                        <Link to="/cart" className='nav__link'>
+                        </Link>
+                        {
+                            numberOfItems > 0 && <span className='cart__length'>{numberOfItems}</span>
+                        }
+                    </li>
+                </ul>
+                <div className="menu__backdrop">
+                    <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+                    </button>
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <Link to="/" className='menu__link'>Home</Link>
+                        </li>
+                        <li className="menu__list">
+                            <Link to="/books" className='menu__link'>Books</Link>
+                        </li>
+                        <li className="menu__list">
+                            <Link to="/cart" className='menu__link'>Cart</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
