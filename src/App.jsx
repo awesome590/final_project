74a276e1694a1303f13439c6bc958aaad4ffce36
Reece <peardon.reece@g,ail.com>
2026-02-24 16:11:02 -0800

    removed colors from previous use of code, re-organized the footer, and deleted placeholder text from project creation

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,6 @@ function App() {
         <Router>
             <Nav/>
             <Home/>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
             <Footer/>
         </Router>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 function Footer() {
     return (
         <footer>
-            <figure className='footer_logo'>
-                <img src='' alt='Logo' />
-            </figure>
             <div className='footer_links'>
                 <Link to='/'>Home</Link>
                 <Link to='/'>Home</Link>
                 <Link to='/'>Home</Link>
             </div>
+            <figure className='footer_logo'>
+                <img src='' alt='Logo' />
+            </figure>
         </footer>
     )
 }
