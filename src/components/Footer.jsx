e6a39c63d51c99119f22003826fce478daeecdb5
Reece Peardon <peardon.reece@gmail.com>
2026-02-25 21:52:45 -0800

    changed footer links from placeholders

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
         <footer>
             <div className='footer_links'>
                 <Link to='/'>Home</Link>
-                <Link to='/'>Home</Link>
-                <Link to='/'>Home</Link>
+                <Link to='/'>Items</Link>
+                <Link to='/'>About</Link>
             </div>
             <figure className='footer_logo'>
                 <img src='' alt='Logo' />
